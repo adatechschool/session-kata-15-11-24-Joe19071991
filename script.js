2d8184b322ebde568e4bca0784094c440aa62b60
Joe19071991 <lauranove66@gmail.com>
2024-11-15 10:59:06 +0100

    ajout-du-dictionnaire

--- script.js
+++ script.js
@@ -5,3 +5,32 @@ function getLatinCharacterList(texte) {
 const mot = "hello,world";
 const array = getLatinCharacterList(mot);
 console.log(array);
+
+const latinToMorse = {
+  A: ".-",
+  B: "-...",
+  C: "-.-.",
+  D: "-..",
+  E: ".",
+  F: "..-.",
+  G: "--.",
+  H: "....",
+  I: "..",
+  J: ".---",
+  K: "-.-",
+  L: ".-..",
+  M: "--",
+  N: "-.",
+  O: "---",
+  P: ".--.",
+  Q: "--.-",
+  R: ".-.",
+  S: "...",
+  T: "-",
+  U: "..-",
+  V: "...-",
+  W: ".--",
+  X: "-..-",
+  Y: "-.--",
+  Z: "--..",
+};
